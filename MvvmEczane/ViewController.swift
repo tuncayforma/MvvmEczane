d9fca2b56ee14b06c3d59c63de7451772d86d4e1
Tuncay FORMA <tuncayforma@hotmail.com>
2022-05-17 16:46:26 +0300

    Initial Commit

---
+++ MvvmEczane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MvvmEczane
+//
+//  Created by Tuncay FORMA on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
